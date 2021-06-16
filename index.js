d0966cf6b0f104ff3011f3a2985574184483881b
Hanyeol Cho <hanyeol.cho@gmail.com>
2021-06-16 10:48:28 +0900

    Add invoke_app function

--- index.js
+++ index.js
@@ -23,11 +23,35 @@ var module = (function() {
             });
         },
 
+        invoke_app: function(app, script, params) {
+            return new Promise(function(resolve, reject) {
+                const request_id = (Math.random() * 10000).toFixed(0);
+
+                global["actions__on_request_" + request_id] = function(result) { 
+                    if (result["callback"] === "resolve") {
+                        resolve(result);
+                    } else {
+                        reject(result);
+                    }
+
+                    delete global["actions__on_request_" + request_id];
+                }
+
+                controller.action("script", Object.assign(params || {}, {
+                    "script": script,
+                    "app": app,
+                    "routes-to-app": "yes",
+                    "return-script": "actions__on_request_" + request_id,
+                    "return-subview": $data["SUBVIEW"]
+                }));
+            });
+        },
+
         resolve: function(params, data) {
             controller.action("script", Object.assign(data || {}, {
                 "script": params["return-script"],
                 "subview": params["return-subview"],
-                "routes-to-topmost": params["routes-to-topmost"] || "no",
+                "routes-to-topmost": params["return-to-topmost"] || "no",
                 "app": params["source-app"] || "",
                 "routes-to-app": params["source-app"] ? "yes" : "no",
                 "callback": "resolve"
@@ -38,7 +62,7 @@ var module = (function() {
             controller.action("script", Object.assign(error || {}, {
                 "script": params["return-script"],
                 "subview": params["return-subview"],
-                "routes-to-topmost": params["routes-to-topmost"] || "no",
+                "routes-to-topmost": params["return-to-topmost"] || "no",
                 "app": params["source-app"] || "",
                 "routes-to-app": params["source-app"] ? "yes" : "no",
                 "callback": "reject"
